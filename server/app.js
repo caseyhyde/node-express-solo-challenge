ea40f2b644c51390ea90f3862e702f790cad07ec
Casey Hyde <hyde.casey@gmail.com>
2016-11-14 14:23:56 -0600

    Initial commit, server appears to connect in port 3000

--- server/app.js
+++ server/app.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+
+app.get('/', index);
+app.use(express.static('server/public'));
+
+app.listen(3000);
+console.log("Listening on port: 3000");
+
+
 // initial jokes provided by the client
 var jokes = [
   {
