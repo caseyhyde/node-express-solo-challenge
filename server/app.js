b26de48801903594b4d61a18a969a41499a10721
Casey Hyde <hyde.casey@gmail.com>
2016-11-14 15:49:40 -0600

    finished

--- server/app.js
+++ server/app.js
@@ -3,8 +3,22 @@ var app = express();
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.get('/', index);
 app.use(express.static('server/public'));
+
+app.get('/jokes', function(req, res) {
+  res.send(jokes);
+  console.log("data send");
+});
+
+app.post('/jokes', function(req, res) {
+  jokes.push(req.body);
+  console.log(jokes);
+  console.log("Received new joke from client, pushing to array: ", req.body);
+});
 
 app.listen(3000);
 console.log("Listening on port: 3000");
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,50 @@
-$(document).ready(function(){
+var jokes = [];
 
-console.log("Hello world!");
-var x = 100;
+$(document).ready(function() {
+  getJokes();
 
+  $('button').on('click', function(event) {
+    event.preventDefault();
+    sendJokes();
+    getJokes();
+    appendJokes(jokes);
+  });
+  //Get jokes from server
+  function getJokes() {
+    $.ajax({
+      type: "GET",
+      url: "/jokes",
+      success: function(data) {
+        jokes = data;
+        console.log("You got these jokes from the server: ", data);
+        appendJokes(data);
+      },
+      error: function() {
+        alert("Something went wrong!");
+      }
+    });
+  }
+  function sendJokes() {
+    var newJoke = {
+      whoseJoke: $('#joker').val(),
+      jokeQuestion: $('#newJoke').val(),
+      punchLine: $('#punchline').val()
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/jokes',
+      data: newJoke,
+      success: function() {
+        console.log("Joke sent!");
+      }
+    });
+  }
+  function appendJokes(array) {
+    $('#jokes').empty();
+    for (var i = 0; i < array.length; i++) {
+      $('#jokes').append('<div class="joke"><h2>' + array[i].whoseJoke +
+      ':</h2><h3>' + array[i].jokeQuestion + '</h3><p>' + array[i].punchLine +
+      '</p></div>');
+    }
+  }
 });
